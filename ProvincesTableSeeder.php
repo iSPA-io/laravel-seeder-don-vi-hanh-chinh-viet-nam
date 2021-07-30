4a5364b6fd9cfdb6ffb9eda4d623af7729320937
Nguyen Minh Vuong <cauvongtrongdembt@gmail.com>
2021-07-30 16:36:45 +0700

    first commit

---
+++ ProvincesTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Provinces;
+use Illuminate\Database\Seeder;
+
+class ProvincesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      $provinces = [
+        [
+          'id',
+          'uuid',
+          'uid',
+          'name',
+          'global_name',
+          'slug',
+          'type',
+          'parent_id',
+          'maps_id',
+          'longitude',
+          'latitude',
+          'priority',
+          'config',
+          'status',
+          'created_at', 'updated_at', 'deleted_at'
+        ],
+      ];
+
+      Provinces::insert($provinces);
+    }
+}
