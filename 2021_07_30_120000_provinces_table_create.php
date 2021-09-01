d5f9dd37bfb05713cab4161f04b720e828a04ea5
Nguyen Minh Vuong <cauvongtrongdembt@gmail.com>
2021-09-01 22:27:42 +0700

    Update 2021_07_30_120000_provinces_table_create.php

--- 2021_07_30_120000_provinces_table_create.php
+++ 2021_07_30_120000_provinces_table_create.php
@@ -16,21 +16,23 @@ class ProvincesTableCreate extends Migration
       Schema::create('provinces', function (Blueprint $table) {
         $table->id();
         $table->uuid('uuid')->nullable()->unique();
-        $table->char('uid', 10)->nullable()->unique();
-        $table->string('name')->nullable()->comment('Local name');
-        $table->string('global_name')->nullable();
-        $table->string('slug')->nullable()->comment('Global name slug');
-        $table->tinyInteger('type')->default(0)->comment('Type of Province (country, city, ward...)');
-        $table->integer('parent_id')->references('id')->on('provinces')->onDelete('cascade');
-        //  Uncomment this line if you haven't used any language table
-        $table->integer('language_id')->references('id')->on('languages')->onDelete('cascade');
-
-        $table->string('maps_id')->nullable()->comment('Google Maps ID - Dev only');
-        $table->char('longitude')->nullable();
-        $table->char('latitude')->nullable();
+        $table->string('uid', 10)->nullable()->unique();
+        $table->string('name')->nullable()->index();
+        $table->string('global_name')->nullable()->index();
+        $table->string('slug')->nullable()->index();
+        $table->tinyInteger('type')->default(0);
+        $table->foreignId('parent_id')->nullable()
+          ->references('id')->on('provinces')->onDelete('set null');
+        // Please change it
+        $table->foreignId('language_id')->nullable()->comment('Default language of country')
+          ->references('id')->on('languages')->onDelete('set null');
+        $table->string('maps_id')->nullable();
+        $table->string('longitude')->nullable();
+        $table->string('latitude')->nullable();
         $table->tinyInteger('priority')->default(0);
-        $table->longText('config')->nullable()->comment('json object configure');
-        $table->boolean('status')->default(false);
+        $table->text('config')->nullable();
+        // Please change it
+        $table->foreignId('status_id')->nullable()->references('id')->on('enumerates')->onDelete('set null');
         $table->timestamps();
         $table->softDeletes();
       });
